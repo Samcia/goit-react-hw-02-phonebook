1a2d4f1d1d4727e6fd69f8a6f7964af8d6278653
Samcia <samantta689@gmail.com>
2023-09-04 16:12:52 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import { Filter } from './Filter';
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
 
-class App extends Component {
+export class App extends Component {
   state = {
     contacts: [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
