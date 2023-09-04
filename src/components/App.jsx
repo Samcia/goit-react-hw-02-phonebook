5898a0b5f0c7a2889ff29b08e4d744d56370dcec
Samcia <samantta689@gmail.com>
2023-09-04 15:58:57 +0200

    big com

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import { ContactsForm } from './ContactsForm';
+import { ContactsList } from './ContactsList';
+import { Filter } from './Filter';
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
 
@@ -12,35 +14,53 @@ class App extends Component {
     ],
     filter: '',
   };
-  addContact = ev => {
-    ev.preventDefault();
+  addContact = event => {
+    event.preventDefault();
 
     const contact = {
       id: nanoid(),
-      name: ev.target.elements.name.value,
-      number: ev.targer.elements.number.value,
+      name: event.target.elements.name.value,
+      number: event.target.elements.number.value,
     };
     if (
       this.state.contacts.some(
         person => person.name.toLowerCase() === contact.name.toLowerCase()
       )
     ) {
-      return alert('${contact.name} exist already in contacts');
+      return alert('${contact.name} already exist');
     }
 
-    this.setState(preventState => ({
-      contacts: [...preventState.contacts, contact],
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
     }));
 
-    ev.target.reset();
+    event.target.reset();
   };
 
+  filterContacts = event => {
+    this.setState({
+      filter: event.target.value,
+    });
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
   render() {
+    const { contacts, filter } = this.state;
     return (
       <>
         <h1>Phonebook</h1>
         <ContactsForm addContact={this.addContact} />
         <h2>Contacts</h2>
+        <Filter filterContacts={this.filterContacts} />
+        <ContactsList
+          contacts={contacts}
+          filter={filter}
+          deleteContact={this.deleteContact}
+        />
       </>
     );
   }
--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -7,27 +7,31 @@ export const ContactsForm = ({ addContact }) => {
       <label>
         Name
         <input
+          className="form__input"
           type="text"
           name="name"
           id=""
-          className="form_input"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
+          placeholder="Write your name"
         />
       </label>
       <label>
         Number
         <input
+          className="form__input"
           type="tel"
           name="number"
-          className="form_input"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
+          placeholder="Write your number"
         />
       </label>
-      <button className="form_button">Add contact</button>
+      <button className="form__button" type="submit">
+        Add contact
+      </button>
     </form>
   );
 };
